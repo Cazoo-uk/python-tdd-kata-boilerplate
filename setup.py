5959da4cd6ebd16809d0dcc9be3c2670c49abbb0
Bob Gregory <bob.gregory@cazoo.co.uk>
2022-01-17 18:05:13 +0000

    Add bumpf

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+setup(
+    name="cazoo-tdd-kata",
+    version="0.1",
+    packages=find_packages(),
+)
---
+++ tests/test_adder.py
@@ -0,0 +1,6 @@
+import adder
+
+print(adder.__dir__())
+
+def test_can_add_two_numbers():
+    assert adder.add(1, 2) == 3
